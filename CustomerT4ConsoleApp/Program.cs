b52027f86148e4beb3bdbe358710b1d08e54ad70
0xack13 <0xack13@gmail.com>
2014-08-11 21:44:05 +0300

    Using T4 Template

--- CustomerT4ConsoleApp/Program.cs
+++ CustomerT4ConsoleApp/Program.cs
@@ -16,6 +16,23 @@ namespace CustomerT4ConsoleApp
     {
         static void Main(string[] args)
         {
+            //Initialize the customer information
+            var customer = new Customer
+            {
+                Name = "Jeffery",
+                LastLoginDate = new DateTime(2014, 11, 2, 19, 23, 32)
+            };
+
+            //Using the T4 template
+            var template = new EmailTemplate();
+            template.Session = new Dictionary<string, object>()
+            {
+                { "Customer", customer }
+            };
+
+            template.Initialize();
+            Console.WriteLine(template.TransformText());
+            Console.ReadLine();
         }
     }
 }
