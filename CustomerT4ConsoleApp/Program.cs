f3cfd4f6c47d01ed2640596e2c2fdf5403a79ca2
0xack13 <0xack13@gmail.com>
2014-08-11 21:38:47 +0300

    Add customer class

--- CustomerT4ConsoleApp/Program.cs
+++ CustomerT4ConsoleApp/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace CustomerT4ConsoleApp
 {
+    public class Customer
+    {
+        public string Name { get; set; }
+        public DateTime LastLoginDate { get; set;}
+    }
+
     class Program
     {
         static void Main(string[] args)
