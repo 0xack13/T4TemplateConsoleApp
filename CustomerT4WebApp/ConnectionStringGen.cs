6a6e3e8fb06ba2110674d038020eef99f116dc97
0xack13 <0xack13@gmail.com>
2014-08-14 12:11:04 +0300

    Connection String C# Generator using T4

---
+++ CustomerT4WebApp/ConnectionStringGen.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+using System.Configuration;
+
+public static class ConnectionStrings
+{
+
+	public static string DefaultConnection
+	{
+		get { return ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString; }
+	}
+	public static string MainConnectionString
+	{
+		get { return ConfigurationManager.ConnectionStrings["MainConnectionString"].ConnectionString; }
+	}
+}
